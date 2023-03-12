e43bc541d315983ea49b76ec92715aea223215da
Kuba <urbanski.jakub77@gmail.com>
2023-03-13 00:32:45 +0100

    fixed an error v3

--- Lab02/z3.py
+++ Lab02/z3.py
@@ -5,23 +5,25 @@ from sklearn.metrics import confusion_matrix
 from sklearn import datasets
 
 iris = sklearn.datasets.load_iris()
-xu = []
-yu = []
-xt = []
-yt = []
 
-for i in range(0, iris['target'].size):
-    if random.random() > 0.2:
-        xu.append(iris['data'][i])
-        yu.append(iris['target'][i])
-    else:
-        xt.append(iris['data'][i])
-        yt.append(iris['target'][i])
+for _ in range(5):
+      xu = []
+      yu = []
+      xt = []
+      yt = []
 
-neuron = Perceptron(tol = 1e-3, max_iter = 20)
-neuron.fit(xu, yu)
-print("Accuracy: " + str(neuron.score(xt, yt)))
+      for i in range(0, iris['target'].size):
+          if random.random() > 0.2:
+              xu.append(iris['data'][i])
+              yu.append(iris['target'][i])
+          else:
+              xt.append(iris['data'][i])
+              yt.append(iris['target'][i])
 
-predicted = neuron.predict(xt)
-confusion_matrix_model = confusion_matrix(yt, predicted)
-print("Confusion matrix:\n" + confusion_matrix_model)
+      neuron = Perceptron(tol = 1e-3, max_iter = 20)
+      neuron.fit(xu, yu)
+      print("Accuracy: " + str(round(neuron.score(xt, yt), 3)))
+
+      predicted = neuron.predict(xt)
+      confusion_matrix_model = confusion_matrix(yt, predicted)
+      print("Confusion matrix:\n" + str(confusion_matrix_model))
